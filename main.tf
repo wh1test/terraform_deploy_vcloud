3e54df28f38585fcddb1809e601b23929445bcf7
besmart <rvv@besmart.by>
2020-04-26 14:40:05 +0300

    Added vars and timeout

--- main.tf
+++ main.tf
@@ -1,24 +1,27 @@
 provider "vcd" {
-  user     = var.admin_user
-  password = var.admin_password
+  user  = var.vcd_user
+  password  = var.vcd_pass
   org      = var.org
-  vdc      = var.org
+  vdc      = var.vdc
   url      = var.url
+  allow_unverified_ssl = true
+  max_retry_timeout = "240"
 }
 
-resource "vcd_vapp_vm" var.vcd_vapp_name {
+resource "vcd_vapp_vm" "vApp_k8s" {
   vapp_name     = var.vcd_vapp_name
-  name          = var.vcd_vm_name
+  name          = var.vm_name
   computer_name = var.vm_name
   catalog_name  = var.template_folder
   template_name = var.template_name
   memory        = var.vm_memory
   cpus          = var.vm_cpus
   cpu_cores     = var.vm_cpu_cores
+  power_on      = var.vm_power_on
 
   metadata = {
     role    = var.vm_role
-    env     = vm.vm_env
+    env     = var.vm_env
     version = var.vm_version
     my_key  = "my value"
   }
@@ -26,7 +29,7 @@ resource "vcd_vapp_vm" var.vcd_vapp_name {
   network {
     type               = var.vm_network_type
     name               = var.vm_network_name
-    ip_allocation_mode = var.ip_allocation_mode
+    ip_allocation_mode = var.net_ip_allocation_mode
     ip                 = var.net_ip
     is_primary         = var.net_is_primary
   }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,43 @@
-variable "admin_user" {}
-variable "admin_password" {}
-variable "org" {}
-variable "vdc" {}
-variable "url" {}
-variable "vcd_vapp_name" {}
+variable "vcd_user" {}
+variable "vcd_pass" {}
+
+variable "org" {
+  default = "193331498"
+}
+variable "vdc" {
+  default = "193331498_001"
+}
+variable "url" {
+  default = "https://vmw.a1.by/api"
+}
+variable "vcd_vapp_name" {
+  default = "vApp_k8s"
+}
 variable "vm_name" {}
-variable "template_folder" {}
-variable "template_name" {}
+variable "template_folder" {
+  default = "templates"
+}
+variable "template_name" {
+  default = "c8-k8s-node-tmpl-a1"
+}
 
 
-variable "vm_memory" {}
-variable "vm_cpus" {}
-variable "vm_cpu_cores" {}
-variable "vm_network_name" {}
+variable "vm_memory" {
+  default = 2048
+}
+variable "vm_cpus" {
+  default = 1
+}
+variable "vm_cpu_cores" {
+  default = 1
+}
+variable "vm_power_on" {
+  default = true
+}
+
+variable "vm_network_name" {
+  default = "dmz-net"
+}
 variable "vm_network_type" {
   default = "org"
 }
@@ -25,10 +50,14 @@ variable "net_ip" {
 variable "net_is_primary" {
   default = true
 }
-variable "vm_role" {}
+variable "vm_role" {
+  default = "k8s-node"
+}
 variable "vm_env" {
   default = "stage"
 }
-variable "vm_version" {}
+variable "vm_version" {
+  default = "1.0"
+}
 
 
